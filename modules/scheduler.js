48fa828eae57f1e3d36e2277c23658ec546272f1
Ryan Clark <ryan@Ryans-MBP.home>
2016-01-24 21:16:54 -0800

    Added initial scheduler

--- modules/scheduler.js
+++ modules/scheduler.js
@@ -3,7 +3,7 @@ var rfCon = require("./rfcontroller.js");
 
 (function(){
     // Second, Min, Hour, Month, Day Of Week
-  var sunriseJob = schedule.scheduleJob('00 10 21 * * *', function(){
+  var sunriseJob = schedule.scheduleJob('00 00 07 * * *', function(){
       console.log('Sunrise Schedule Fired');
       rfCon.lightNightOff();
       // Debounce
@@ -12,7 +12,7 @@ var rfCon = require("./rfcontroller.js");
       },1000);
   });
 
-  var sunsetJob = schedule.scheduleJob('00 11 21 * * *', function(){
+  var sunsetJob = schedule.scheduleJob('00 30 17 * * *', function(){
     console.log('Sunset Schedule Fired');
     rfCon.lightDayOff();
     // Debounce
